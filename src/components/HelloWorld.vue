ac6b0f563c287c132d2c29e84ae0600cbe2229e7
Pavel Zlobin <mickon196@yandex.ru>
2021-10-03 21:01:38 +0700

    test 3d model

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,119 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <canvas id="container"></canvas>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import * as THREE from 'three'
+  import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+  import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
+
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    methods: {
+      gltfload() {
+        const canvas = document.querySelector('#container');
+        const renderer = new THREE.WebGLRenderer({canvas});
+        renderer.setSize(window.innerWidth - 5, window.innerHeight - 5)
+
+        const fov = 45;
+        const aspect = window.innerWidth / window.innerHeight;  // the canvas default
+        const near = 0.1;
+        const far = 100;
+        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        camera.position.z = 5
+
+        // позволяет нам покрутиться вокруг полотна
+        const controls = new OrbitControls(camera, canvas);
+        controls.target.set(0, 5, 0);
+        controls.update();
+
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color('#ffffff');
+
+        function frameArea(sizeToFitOnScreen, boxSize, boxCenter, camera) {
+          const halfSizeToFitOnScreen = sizeToFitOnScreen * 1;
+          const halfFovY = THREE.MathUtils.degToRad(camera.fov * .5);
+          const distance = halfSizeToFitOnScreen / Math.tan(halfFovY);
+          // вычисляем единичный вектор, указывающий в направлении, в котором сейчас находится камера
+          // в плоскости xz от центра прямоугольника
+          const direction = (new THREE.Vector3())
+            .subVectors(camera.position, boxCenter)
+            .multiply(new THREE.Vector3(45, 0, 1))
+            .normalize();
+
+          camera.position.copy(direction.multiplyScalar(distance).add(boxCenter));
+
+          // расстояние ближнего и дальшего конца бокса
+          camera.near = boxSize / 100;
+          camera.far = boxSize * 100;
+
+          camera.updateProjectionMatrix();
+
+          // камера направлена в центр бокса
+          camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
+        }
+
+        // загружаем модель
+        const gltfLoader = new GLTFLoader();
+        gltfLoader.load('/assets/scene.gltf', (gltf) => {
+          const root = gltf.scene;
+          scene.add(root);
+
+          // все файлы от корня и ниже подгружает вроде ????
+          const box = new THREE.Box3().setFromObject(root);
+
+          const boxSize = box.getSize(new THREE.Vector3()).length();
+          const boxCenter = box.getCenter(new THREE.Vector3());
+
+          // вставляем камеру на сцену
+          frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
+
+          // тут обрабатываем новый размер
+          controls.maxDistance = boxSize * 10;
+          controls.target.copy(boxCenter);
+          controls.update();
+        });
+
+        function resizeRendererToDisplaySize(renderer) {
+          const canvas = renderer.domElement;
+          const width = canvas.clientWidth;
+          const height = canvas.clientHeight;
+          const needResize = canvas.width !== width || canvas.height !== height;
+          if (needResize) {
+            renderer.setSize(width, height, false);
+          }
+          return needResize;
+        }
+
+        function render() {
+          if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+          }
+
+          renderer.render(scene, camera);
+
+          requestAnimationFrame(render);
+        }
+
+        requestAnimationFrame(render);
+      }
+    },
+    mounted() {
+      this.gltfload()
+    }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+<style>
+  * {
+    margin: 0;
+  }
 </style>
